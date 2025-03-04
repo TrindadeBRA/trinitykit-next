0c7a85e0c324215790bb2527679a5fbeb135fd08
Lucas Trindade <trindadebra@gmail.com>
2025-03-04 19:58:50 -0300

    tailwind add components

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  images: {
+    unoptimized: true,
+    domains: ['tailwindcss.com'],
+  },
   /* config options here */
 };
 
