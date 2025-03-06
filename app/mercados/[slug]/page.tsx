10ccecb3d681f5818d014956cffa35416661dfd5
Lucas Trindade <trindadebra@gmail.com>
2025-03-05 21:41:13 -0300

    chore: add button

--- app/mercados/[slug]/page.tsx
+++ app/mercados/[slug]/page.tsx
@@ -1,13 +1,15 @@
+// app/mercado/[slug]/page.tsx
 import IntroProductLine from "@/src/components/IntroProductLine";
 import { getGetSegmentSlugUrl, getSegmentSlugResponse } from "@/src/services/api";
 import customFetch from "@/src/services/custom-fetch";
 import { GetSegmentSlug200DataItem, GetSegmentSlug200DataItemCategoriesItem, GetSegmentSlug200DataItemCategoriesItemProductsItem } from "@/src/services/model";
+import { Metadata } from 'next';
 
 interface MercadoProps {
   slug?: string;
 }
 
-//traduzir slugs
+// Traduzir slugs
 export const slugToName = {
   "plasticos-elastomeros": "Plásticos e Elastoméricos",
   "cosmeticos": "Cosméticos",
@@ -25,33 +27,36 @@ async function getSegmentsInfo(slug: string): Promise<getSegmentSlugResponse> {
   }
 }
 
+  export async function generateMetadata(
+  { params }: { params: MercadoProps }
+): Promise<Metadata> {
+  const { slug } = params;
+  
+  const segmentName = slug ? slugToName[slug as keyof typeof slugToName] || slug : 'Segmento Desconhecido';
+  const segmentNameLower = segmentName.toLowerCase();
+  
+  return {
+    title: `Tiken - ${segmentName}`,
+    description: `Explore nossa linha de produtos para o mercado de ${segmentNameLower}.`,
+  };
+}
 
-export default async function Page({ params }: { params: Promise<MercadoProps> }) {
-  const { slug } = await params;
-
+export default async function Page({ params }: { params: MercadoProps }) {
+  const { slug } = params;
   if (!slug) {
     return <div>Slug não encontrado</div>;
   }
 
   const response: getSegmentSlugResponse = await getSegmentsInfo(slug);
   const segmentInfo = response.data;
-
-  console.log(">>>>>>>>>>>>>segmentInfo", segmentInfo);
-
-
   if (!segmentInfo) {
     return <div>Segmento não encontrado</div>;
   }
 
-
   return (
     <div>
-      <h1 className="text-4xl font-bold">Mercado - {slug}</h1>
-
       {
         Object.values(segmentInfo).map((product_lines: GetSegmentSlug200DataItem, index: number) => {
-          // console.log(">>>>>>>>>>>>>product_lines", product_lines);
-
           return (
             <div key={product_lines.slug} style={{ flexDirection: index % 2 === 0 ? 'row' : 'row-reverse' }}>
               <IntroProductLine
@@ -60,8 +65,6 @@ export default async function Page({ params }: { params: Promise<MercadoProps> }
                 description={product_lines.description!}
                 imagesUrls={product_lines.images!}
               />
-
-
               <div className="flex gap-4 flex-wrap container mx-auto">
                 {product_lines?.categories?.map((category: GetSegmentSlug200DataItemCategoriesItem) => (
                   <div key={category.slug} className="flex-1 max-w-1/2">
@@ -85,29 +88,23 @@ export default async function Page({ params }: { params: Promise<MercadoProps> }
                   </div>
                 ))}
               </div>
-              
-              
             </div>
           )
         })
       }
       <div className="mb-96"></div>
     </div>
-
   )
 }
 
-
 export async function generateStaticParams() {
-  // const posts = await fetch('https://.../posts').then((res) => res.json())
-
   const posts = [
     { slug: "plasticos-elastomeros" },
     { slug: "cosmeticos" },
     { slug: "adesivos" },
     { slug: "tintas-vernizes" },
   ];
-
+  
   return posts.map((post) => ({
     slug: post.slug,
   }))
