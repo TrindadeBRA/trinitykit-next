66dd47cd33fb7d29ba679a85be947043d9f95a9b
Lucas Trindade <trindadebra@gmail.com>
2025-03-05 21:32:34 -0300

    chore: fix tables

--- app/mercados/[slug]/page.tsx
+++ app/mercados/[slug]/page.tsx
@@ -50,7 +50,7 @@ export default async function Page({ params }: { params: Promise<MercadoProps> }
 
       {
         Object.values(segmentInfo).map((product_lines: GetSegmentSlug200DataItem, index: number) => {
-          console.log(">>>>>>>>>>>>>product_lines", product_lines);
+          // console.log(">>>>>>>>>>>>>product_lines", product_lines);
 
           return (
             <div key={product_lines.slug} style={{ flexDirection: index % 2 === 0 ? 'row' : 'row-reverse' }}>
@@ -60,28 +60,33 @@ export default async function Page({ params }: { params: Promise<MercadoProps> }
                 description={product_lines.description!}
                 imagesUrls={product_lines.images!}
               />
-              
-              {product_lines?.categories?.map((category: GetSegmentSlug200DataItemCategoriesItem) => (
-                <div key={category.slug}>
-                  <h2 className="text-2xl font-semibold">{category.name}</h2>
-                  <table>
-                    <thead>
-                      <tr>
-                        <th>Nome</th>
-                        <th>CAS Number</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {category?.products?.map((product: GetSegmentSlug200DataItemCategoriesItemProductsItem) => (
-                        <tr key={product.id}>
-                          <td>{product.title}</td>
-                          <td>{product.cas_number}</td>
+
+
+              <div className="flex gap-4 flex-wrap container mx-auto">
+                {product_lines?.categories?.map((category: GetSegmentSlug200DataItemCategoriesItem) => (
+                  <div key={category.slug} className="flex-1 max-w-1/2">
+                    <h3 className="text-lg text-center font-semibold mb-2">{category.name}</h3>
+                    <table className="border-collapse border border-gray-200 w-full">
+                      <thead className="bg-gray-100">
+                        <tr>
+                          <th className="border border-gray-300 px-4 py-2">Nome</th>
+                          <th className="border border-gray-300 px-4 py-2">CAS Number</th>
                         </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                </div>
-              ))}
+                      </thead>
+                      <tbody>
+                        {category?.products?.map((product: GetSegmentSlug200DataItemCategoriesItemProductsItem) => (
+                          <tr key={product.id} className="hover:bg-gray-50">
+                            <td className="border border-gray-300 px-4 py-2">{product.title}</td>
+                            <td className="border border-gray-300 px-4 py-2">{product.cas_number}</td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  </div>
+                ))}
+              </div>
+              
+              
             </div>
           )
         })
