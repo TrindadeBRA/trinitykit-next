7d836944ddabe722ca40f12ca50e8aaabc306a22
Lucas Trindade <trindadebra@gmail.com>
2025-03-16 05:23:02 -0300

    fix: linha de produtos base

--- app/linha-de-produtos/[slug]/page.tsx
+++ app/linha-de-produtos/[slug]/page.tsx
@@ -75,25 +75,32 @@ export default async function Page({ params }: { params: Promise<ProductLineProp
         imagesUrls={data.parent?.images || []}
       />
 
-      <div className="flex gap-4 flex-wrap container mx-auto">
+      <div className="flex gap-x-4 gap-y-12 md:gap-y-6 px-8 flex-wrap container mx-auto">
+        
         {data?.subcategories?.map((category: GetProductLineSlug200DataSubcategoriesItem) => {
+          const getMaxWidthClass = (length: number) => {
+            const widthMap: Record<number, string> = {
+              1: "max-w-full",
+              2: "max-w-1/2",
+              3: "max-w-1/3"
+            };
+            return `max-w-full md:${widthMap[length] || "max-w-1/3"}`;
+          };
 
-          const maxWidthClass = data?.subcategories?.length ?
-            data?.subcategories?.length === 1 ? "max-w-full" :
-              data?.subcategories?.length === 2 ? "max-w-1/2" :
-                data?.subcategories?.length === 3 ? "max-w-1/3" :
-                  "max-w-1/3" : ""; // Para 4 ou mais
+          const maxWidthClass = data?.subcategories?.length 
+            ? getMaxWidthClass(data.subcategories.length)
+            : "";
 
           return (
             <div key={category.slug} className={twMerge(
-              "flex-1",
+              "flex-1 w-full md:w-auto",
               maxWidthClass
             )}>
               <h3 className="text-lg text-center font-semibold mb-2">{category.name}</h3>
               <table className="border-collapse border border-gray-200 w-full">
                 <thead className="bg-gray-100">
                   <tr>
-                    <th className="border border-gray-300 px-4 py-2">Nome</th>
+                    <th className="border border-gray-300 px-4 py-2">Produto</th>
                     <th className="border border-gray-300 px-4 py-2">CAS Number</th>
                   </tr>
                 </thead>
