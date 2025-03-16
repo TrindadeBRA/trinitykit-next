c4ab68f0ea66ae72a1e5e8c772053ca7492ba69f
Lucas Trindade <trindadebra@gmail.com>
2025-03-16 01:56:35 -0300

    feat: add gradientbg in menu

--- src/components/LanguageSwitcher/index.tsx
+++ src/components/LanguageSwitcher/index.tsx
@@ -1,7 +1,9 @@
 import { LanguageDescriptor, useLanguageSwitcher } from "@/src/hooks/useLanguageSwitcher";
 import { NextPageContext } from "next";
+import { twMerge } from "tailwind-merge";
 
-export const LanguageSwitcher = ({ context }: { context?: NextPageContext } = {}) => {
+
+export const LanguageSwitcher = ({ context, isGradient }: { context?: NextPageContext; isGradient?: boolean } = {}) => {
   const { currentLanguage, switchLanguage, languageConfig } = useLanguageSwitcher({ context });
 
   if (!languageConfig) {
@@ -9,13 +11,13 @@ export const LanguageSwitcher = ({ context }: { context?: NextPageContext } = {}
   }
 
   return (
-    <div className="text-center notranslate text-base lg:text-2xl flex gap-x-2 mr-4">
+    <div className={twMerge("text-center notranslate text-base lg:text-lg flex gap-x-1 mr-4 font-semibold text-gray-900 border-gray-900", isGradient && "text-white border-white")}>
       {languageConfig.languages.map((ld: LanguageDescriptor) => (
         <span key={`l_s_${ld.name}`}>
           {currentLanguage === ld.name ? (
-            <span className="cursor-not-allowed text-red-500">{ld.name.toUpperCase()}</span>
+            <span className="cursor-not-allowed rounded-lg border p-1.5">{ld.name.toUpperCase()}</span>
           ) : (
-            <a onClick={switchLanguage(ld.name)} className="cursor-pointer">
+            <a onClick={switchLanguage(ld.name)} className={twMerge("cursor-pointer rounded-lg p-1.5 border border-black/60 text-black/60", isGradient && "border-white/60 text-white/60")}>
               {ld.name.toUpperCase()}
             </a>
           )}
--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -5,19 +5,32 @@ import { Dialog, DialogPanel } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import Image from 'next/image'
 import LanguageSwitcher from '../LanguageSwitcher'
+import { twMerge } from 'tailwind-merge'
+import { usePathname } from 'next/navigation'
+import { ArrowRightIcon } from 'lucide-react'
 
 const navigation = [
   { name: 'Home', href: '/' },
   { name: 'Quem Somos', href: '/quem-somos' },
-  { name: 'Nossos Produtos', href: '/nossos-produtos' },
-  { name: 'Blog', href: '/blog' },
+  { name: 'Produtos', href: '/produtos' },
   { name: 'Mercados', href: '/mercados' },
   { name: 'Contato', href: '/contato' },
+  { name: 'Blog', href: '/blog' },
+  { name: 'Portal Inove', href: '/loja', separator: true },
 ]
 
 export default function Navigation() {
+
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
   const [isFixed, setIsFixed] = useState(false)
+  const [isGradient, setIsGradient] = useState(false)
+
+  const pathname = usePathname()
+
+  useEffect(() => {
+    setIsGradient(pathname !== '/')
+  }, [pathname])
+
 
   useEffect(() => {
     const handleScroll = () => {
@@ -31,20 +44,27 @@ export default function Navigation() {
   }, [])
 
   return (
-    <header className={`bg-white fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${isFixed ? 'shadow-lg' : ''}`}>
+    <header className={twMerge(`fixed top-0 left-0 right-0 z-50 transition-all duration-300 
+      ${isFixed ? 'shadow-lg' : ''}
+      ${isGradient ? 'bg-gradient-custom' : 'bg-white'}
+    `)}>
       <nav aria-label="Global" className={`flex items-center justify-between p-6 lg:px-8 transition-all duration-300 ${isFixed ? 'py-4' : 'py-6'}`}>
+
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
             <Image
               alt=""
-              src="/assets/logo-header.png"
+              src={isGradient ? '/assets/logo-header-white.png' : '/assets/logo-header.png'}
               className="h-12 w-auto"
               height={48}
               width={48}
             />
           </a>
         </div>
+
+
+
         <div className="flex lg:hidden">
           <button
             type="button"
@@ -55,18 +75,30 @@ export default function Navigation() {
             <Bars3Icon aria-hidden="true" className="size-6" />
           </button>
         </div>
-        <div className="hidden lg:flex lg:gap-x-12">
+
+        <div className="hidden lg:flex gap-x-4 items-center">
           {navigation.map((item) => (
-            <a key={item.name} href={item.href} className="text-sm/6 font-semibold text-gray-900">
+            <a key={item.name} href={item.href} className={twMerge("text-sm/6 font-semibold text-gray-900", item.separator && "border-l border-gray-900 pl-4", isGradient && "text-white border-white")}>
               {item.name}
             </a>
           ))}
         </div>
-        
-        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <LanguageSwitcher />
+
+        {/* <div className="hidden lg:flex flex-1 justify-end">
+          <a href={"/#contato"} className={twMerge("font-semibold flex items-center gap-x-2 bg-gradient-custom text-white px-3 py-2 rounded-lg", isGradient && "bg-white text-gray-900")}>fale conosco <ArrowRightIcon className="size-4" /></a>
+        </div> */}
+
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end lg:items-center">
+          <a href={"/#contato"} className={twMerge("mr-4 font-semibold flex items-center gap-x-2 bg-gradient-custom text-white px-3 py-2 rounded-lg", isGradient && "bg-white text-gray-900")}>fale conosco <ArrowRightIcon className="size-4" /></a>
+          <LanguageSwitcher isGradient={isGradient} />
         </div>
+
       </nav>
+
+
+
+
+
       <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden z-50">
         <div className="fixed inset-0 z-10" />
         <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
@@ -75,7 +107,7 @@ export default function Navigation() {
               <span className="sr-only">Your Company</span>
               <Image
                 alt=""
-                src="/assets/logo-header.png"
+                src={isGradient ? '/assets/logo-header-white.png' : '/assets/logo-header.png'}
                 className="h-8 w-auto"
                 width={32}
                 height={32}
@@ -102,7 +134,7 @@ export default function Navigation() {
                     {item.name}
                   </a>
                 ))}
-                
+
               </div>
               <div className="py-6">
                 <LanguageSwitcher />
