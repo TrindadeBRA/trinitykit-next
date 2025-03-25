d9325ac47aef7dd3e61333086ad7d322dfd07ea4
Lucas Trindade <trindadebra@gmail.com>
2025-03-25 19:00:21 -0300

    feat: OurPurpose added

--- src/components/OurPurpose/index.tsx
+++ src/components/OurPurpose/index.tsx
@@ -1,59 +1,72 @@
 import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/20/solid'
-
-const features = [
-  {
-    name: 'Push to deploy.',
-    description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-    icon: CloudArrowUpIcon,
-  },
-  {
-    name: 'SSL certificates.',
-    description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
-    icon: LockClosedIcon,
-  },
-  {
-    name: 'Database backups.',
-    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
-    icon: ServerIcon,
-  },
-]
+import Image from 'next/image'
 
 export default function OurPurpose() {
   return (
-    <div className="overflow-hidden bg-white py-24 sm:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-          <div className="lg:pt-4 lg:pr-8">
-            <div className="lg:max-w-lg">
-              <h2 className="text-base/7 font-semibold text-indigo-600">Deploy faster</h2>
-              <p className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl">
-                A better workflow
-              </p>
-              <p className="mt-6 text-lg/8 text-gray-600">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque,
-                iste dolor cupiditate blanditiis ratione.
-              </p>
-              <dl className="mt-10 max-w-xl space-y-8 text-base/7 text-gray-600 lg:max-w-none">
-                {features.map((feature) => (
-                  <div key={feature.name} className="relative pl-9">
-                    <dt className="inline font-semibold text-gray-900">
-                      <feature.icon aria-hidden="true" className="absolute top-1 left-1 size-5 text-indigo-600" />
-                      {feature.name}
-                    </dt>{' '}
-                    <dd className="inline">{feature.description}</dd>
-                  </div>
-                ))}
-              </dl>
+    <div className="relative bg-white my-20">
+      <div className="mx-auto max-w-7xl lg:flex lg:justify-between lg:px-8 xl:justify-end">
+        <div className="lg:flex lg:w-1/2 lg:shrink lg:grow-0 xl:absolute xl:inset-y-0 xl:right-1/2 xl:w-1/2">
+          <div className="relative h-80 lg:-ml-8 lg:h-auto lg:w-full lg:grow xl:ml-0">
+            <Image
+              alt=""
+              src="/assets/images/our-purpose.webp"
+              className="absolute inset-0 size-full bg-gray-50 object-cover rounded-lg"
+              width={1000}
+              height={1000}
+            />
+          </div>
+        </div>
+        <div className="px-6 lg:contents">
+          <div className="mx-auto max-w-2xl pt-16 pb-24 sm:pt-20 sm:pb-32 lg:mr-0 lg:ml-8 lg:w-full lg:max-w-lg lg:flex-none lg:pt-32 xl:w-1/2">
+            <p className="text-base/7 font-semibold text-[#0399c4]">Sobre Nós</p>
+            <h2 className="mt-2 text-5xl font-semibold tracking-tight text-pretty text-[#515151]">
+              Nosso propósito
+            </h2>
+            <p className="mt-6 text-xl text-[#515151]">
+              Criamos conexões e soluções através da química para inovar o mundo.
+            </p>
+            <div className="mt-10">
+              <h2 className="mt-2 text-2xl font-semibold tracking-tight text-pretty text-[#515151]">
+                O que fazemos?
+              </h2>
+              <ul className="text-xl text-[#515151] list-disc list-inside mt-4 space-y-2">
+                <li>
+                  Garantimos a satisfação total em todos os aspectos do negócio.
+                </li>
+                <li>
+                  Gestão em distribuição e representação de especialidades químicas.
+                </li>
+                <li>
+                  Criamos conexões através de suporte técnico/comercial.
+                </li>
+                <li>
+                  Capacitação técnica dos nossos clientes e parceiros, visando as exigências do futuro.
+                </li>
+              </ul>
+            </div>
+            <div className="mt-10">
+              <h2 className="mt-2 text-2xl font-semibold tracking-tight text-pretty text-[#515151]">
+                Nossas Atitudes
+              </h2>
+              <ul className="text-xl text-[#515151] list-disc list-inside mt-4 space-y-2">
+                <li>
+                  Respeito pelas pessoas em primeiro lugar.
+                </li>
+                <li>
+                  Foco no cliente.
+                </li>
+                <li>
+                  Responsabilidade e ética.
+                </li>
+                <li>
+                  Boas práticas da LGPD.
+                </li>
+                <li>
+                  Paixão pelo que fazemos.
+                </li>
+              </ul>
             </div>
           </div>
-          <img
-            alt="Product screenshot"
-            src="https://tailwindcss.com/plus-assets/img/component-images/dark-project-app-screenshot.png"
-            width={2432}
-            height={1442}
-            className="w-[48rem] max-w-none rounded-xl ring-1 shadow-xl ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
-          />
         </div>
       </div>
     </div>
