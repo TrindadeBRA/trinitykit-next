eae0133cfe2dad4d050ffc8522930f050d31e0d4
Lucas Trindade <trindadebra@gmail.com>
2025-03-25 20:26:21 -0300

    feat: our blog added

--- src/components/OurBlog/index.tsx
+++ src/components/OurBlog/index.tsx
@@ -1,7 +1,7 @@
 const posts = [
     {
         id: 2,
-        title: 'Boost your conversion rate',
+        title: 'Boost your conversion rate, Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna.',
         href: '#',
         description:
             'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
@@ -51,19 +51,22 @@ const posts = [
 
 export default function OurBlog() {
     return (
-        <div className="bg-white py-24 sm:py-32">
+        <div className="bg-white py-16">
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
                 <div className="mx-auto max-w-2xl text-center">
-                    <h2 className="text-4xl font-semibold tracking-tight text-balance text-gray-900 sm:text-5xl">
-                        From the blog
+                    <p className="text-base/7 font-semibold text-[#0399c4] text-center" data-aos="zoom-in">Fique por Dentro</p>
+
+                    <h2 className="text-4xl font-semibold tracking-tight text-balance text-gray-900 sm:text-5xl" data-aos="zoom-in">
+                        Nosso Blog
                     </h2>
-                    <p className="mt-2 text-lg/8 text-gray-600">Learn how to grow your business with our expert advice.</p>
+                    <p className="mt-2 text-lg/8 text-gray-600 text-center" data-aos="zoom-in">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam.</p>
                 </div>
                 <div className="mx-auto mt-16 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
                     {posts.map((post) => (
                         <article
                             key={post.id}
                             className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 px-8 pt-80 pb-8 sm:pt-48 lg:pt-80"
+                            data-aos="zoom-in"
                         >
                             <img alt="" src={post.imageUrl} className="absolute inset-0 -z-10 size-full object-cover" />
                             <div className="absolute inset-0 -z-10 bg-linear-to-t from-gray-900 via-gray-900/40" />
@@ -73,18 +76,10 @@ export default function OurBlog() {
                                 <time dateTime={post.datetime} className="mr-8">
                                     {post.date}
                                 </time>
-                                <div className="-ml-4 flex items-center gap-x-4">
-                                    <svg viewBox="0 0 2 2" className="-ml-0.5 size-0.5 flex-none fill-white/50">
-                                        <circle r={1} cx={1} cy={1} />
-                                    </svg>
-                                    <div className="flex gap-x-2.5">
-                                        <img alt="" src={post.author.imageUrl} className="size-6 flex-none rounded-full bg-white/10" />
-                                        {post.author.name}
-                                    </div>
-                                </div>
                             </div>
                             <h3 className="mt-3 text-lg/6 font-semibold text-white">
-                                <a href={post.href}>
+                                {/* Limitar a no amx 2 linhas */}
+                                <a href={post.href} className="line-clamp-2">
                                     <span className="absolute inset-0" />
                                     {post.title}
                                 </a>
